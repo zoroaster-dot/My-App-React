1b00af475e8be32282f8f806bb896a141b9e608d
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2022-09-05 08:27:17 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-undef */
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li href="QTemu.js">QTemu</li> |
+        <li href="CreateMeetup.html">Create Meetup</li> |
+        <li href="Explore.html">Explore</li> | |
+      </ul>
+
+
+      <h2>
+      Hacktiv8 Meetup
+    </h2>
+    
+    <p>
+      Location  :   Jakarta, Indonesia <br></br>
+      Member    :   1,500 <br></br>
+      Organizer :   Muhammad Ifka Nabilah <br></br>
+      <button><input type="submit" value="Join Us"></input></button>
+    </p>
+
+    <h4>
+      Next Meetup
+    </h4>
+
+    <p align="left">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    <h4>
+      About MeetUp
+    </h4>
+
+    <p align="left">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <h4>
+      Member
+      <h5 align = "right">See All</h5>
+    </h4>
+
+    <div class="kotak kotak1" align="left">
+      Organizer <br></br>
+      Muhammad Ifka Nabilah <br></br>
+      4other
+    </div>
+    <h4>
+      Past MeetUp
+      <h5 align = "right">See All</h5>
+    </h4>
+    <div class="kotak kotak2" align="left">
+      2 November 2022 <br></br>
+      Lorem ipsum dolor sit ameth <br></br>
+      <button><input type="submit" value="View"></input></button>
+    </div>
+    
+
+    <div class="tengah">
+      <div class="kotak kotak2" align="left">
+        3 November 2022 <br></br>
+        Lorem ipsum dolor sit amet <br></br>
+        <button><input type="submit" value="View"></input></button>
+      </div>
+      <div class="kotak kotak4" align="left">
+        4 November 2022 <br></br>
+        Lorem ipsum dolor sit amet<br></br>
+        <button><input type="submit" value="View"></input></button>
+      </div>
+    </div>  
+  </div>
+  );
+}
+
+export default App;
