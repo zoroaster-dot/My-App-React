b3ea9d924a7a229ab379d5f7879e0da88afe4dce
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2022-09-05 08:29:17 +0700

    Create QTemu.js

---
+++ component/QTemu.js
@@ -0,0 +1,28 @@
+import React, { components } from "react";
+
+class App extends components {
+  render() {
+    return <ul>
+    <li href="QTemu.js">QTemu</li> |
+    <li href="CreateMeetup.html">Create Meetup</li> |
+    <li href="Explore.html">Explore</li> | |
+    </ul>;
+
+  <img src="/img/logoHacktiv8.jpg" className={StyleSheet.logo} alt="logo"/>;
+    
+
+    <h2>
+      Hacktiv8 Meetup
+    </h2>;
+    
+    <p>
+      Location  :   Jakarta, Indonesia
+      Member    :   1,500
+      Organizer :   Muhammad Ifka Nabilah
+    </p>;
+
+  }
+  
+}
+
+export default App;
